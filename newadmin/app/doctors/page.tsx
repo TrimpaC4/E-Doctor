427d50c84d02eaf8287d660e4db949c7f14c97a9
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-18 17:19:01 +0100

    navbar done

---
+++ newadmin/app/doctors/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Homee from '../homee/page';
+import Navbar from '../navbar/page';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      
+    </div>
+  )
+}
+
+export default Page
