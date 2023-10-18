dd45095ef56578f7bf843c01b3d1fe5a3089b584
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2023-10-18 15:36:18 +0100

    some pages

--- client/app/login/Form.tsx
+++ client/app/login/Form.tsx
@@ -1,94 +1,96 @@
+"use client"
 import React, { useState } from "react";
-
-import { RootState, AppDispatch } from "../redux/store";
-import { useSelector, useDispatch } from "react-redux";
-import { doctorLogin } from "../redux/doctorSlice";
-import { loginPatient } from "../redux/patientSlice";
-// import { useRouter } from "next/router";
-import { getOneDoctor } from "../redux/doctorSlice";
-import { getOnePatient } from "../redux/patientSlice";
-import { toast } from "react-toastify";
 import "./style.css";
+import { RootState, AppDispatch } from "../../src/redux/store";
+import { useSelector, useDispatch } from "react-redux";
+import { doctorLogin } from "../../src/redux/doctorSlice";
+import { loginPatient } from "../../src/redux/patientSlice";
+import { useRouter } from "next/navigation";
+import { getOneDoctor } from "../../src/redux/doctorSlice";
+import { getOnePatient } from "../../src/redux/patientSlice";
+import { toast } from "react-toastify";
 
 function Form() {
-//   const user = useSelector((state: RootState) => state.doctor);
-//   const dispatch: AppDispatch = useDispatch();
-//   const router = useRouter();
+  const router = useRouter()
+  const user = useSelector((state: RootState) => state.doctor);
+  const dispatch: AppDispatch = useDispatch();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userType, setUserType] = useState("");
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    // try {
-    //   e.preventDefault();
-    //   if (userType === "doctor") {
-    //     const res = await dispatch(
-    //       doctorLogin({
-    //         email,
-    //         password,
-    //       })
-    //     );
+  const handleSubmit = async (e: any) => {
+    try {
+      e.preventDefault();
+      if (userType === "doctor") {
+        const res = await dispatch(
+          doctorLogin({
+            email,
+            password,
+          }),
+        );
 
-    //     if (res.payload.token) {
-    //       router.push("/");
-    //       dispatch(getOneDoctor());
-    //       toast.success("Welcome", {
-    //         position: "top-center",
-    //         autoClose: 5000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: false,
-    //         draggable: true,
-    //         progress: undefined,
-    //         theme: "dark",
-    //       });
-    //     } else {
-    //       toast.error("Wrong Password Or Email", {
-    //         position: "top-center",
-    //         autoClose: 5000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: false,
-    //         draggable: true,
-    //         progress: undefined,
-    //         theme: "dark",
-    //       });
-    //     }
-    //   } else if (userType === "patient") {
-    //     const res = await dispatch(
-    //       loginPatient({
-    //         email,
-    //         password,
-    //       })
-    //     );
-    //     if (res.payload.token) {
-    //       dispatch(getOnePatient());
-    //       router.push("/");
-    //       toast.success("Welcome", {
-    //         position: "top-center",
-    //         autoClose: 5000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: false,
-    //         draggable: true,
-    //         progress: undefined,
-    //         theme: "dark",
-    //       });
-    //     } else {
-    //       toast.error("Wrong Password Or Email", {
-    //         position: "top-center",
-    //         autoClose: 5000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: false,
-    //         draggable: true,
-    //         progress: undefined,
-    //         theme: "dark",
-    //       });
-    //     }
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
+        if (res.payload.token) {
+          router.push("/");
+          dispatch(getOneDoctor())
+          toast.success("Welcome", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        } else {
+          toast.error("Wrong Password Or Email", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      }
+      else if (userType === "patient") {
+        const res = await dispatch(
+          loginPatient({
+            email,
+            password,
+          })
+        );
+        if (res.payload.token) {
+          dispatch(getOnePatient())
+          router.push("/");
+          toast.success("Welcome", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        } else {
+          toast.error("Wrong Password Or Email", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="signIn">
@@ -112,83 +114,75 @@ function Form() {
       <div className="divider d-flex align-items-center my-4">
         <p className="text-center fw-bold mx-3 mb-0">Or</p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-outline mb-4">
-          <input
-            type="email"
-            id="form3Example3"
-            className="form-control form-control-lg"
-            placeholder="Enter a valid email address"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <label className="form-label" htmlFor="form3Example3">
-            Email address
-          </label>
-        </div>
-        <div className="form-outline mb-3">
-          <input
-            type="password"
-            id="form3Example4"
-            className="form-control form-control-lg"
-            placeholder="Enter password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <label className="form-label" htmlFor="form3Example4">
-            Password
-          </label>
-        </div>
+      <div className="form-outline mb-4">
+        <input
+          type="email"
+          id="form3Example3"
+          className="form-control form-control-lg"
+          placeholder="Enter a valid email address"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <label className="form-label" htmlFor="form3Example3">
+          Email address
+        </label>
+      </div>
+      <div className="form-outline mb-3">
+        <input
+          type="password"
+          id="form3Example4"
+          className="form-control form-control-lg"
+          placeholder="Enter password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <label className="form-label" htmlFor="form3Example4">
+          Password
+        </label>
+      </div>
 
-        <div className="d-flex justify-content-between align-items-center">
-          <div className="form-check mb-0">
-            <input
-              className="form-check-input me-2"
-              type="checkbox"
-              value=""
-              id="form2Example3"
-            />
-            <label className="form-check-label" htmlFor="form2Example3">
-              Remember me
-            </label>
-          </div>
-          <a href="#!" className="text-body">
-            Forgot password?
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="form-check mb-0">
+          <input
+            className="form-check-input me-2"
+            type="checkbox"
+            value=""
+            id="form2Example3"
+          />
+          <label className="form-check-label" htmlFor="form2Example3">
+            Remember me
+          </label>
+        </div>
+        <a href="#!" className="text-body">
+          Forgot password?
+        </a>
+      </div>
+      <div>
+        <select required onChange={(e: any) => setUserType(e.target.value)}>
+          <option selected>Choose One Please </option>
+          <option value="doctor">Doctor</option>
+          <option value="patient">Patient</option>
+        </select>
+      </div>
+
+      <div className="text-center text-lg-start mt-4 pt-2">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg"
+          style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+          onClick={(e) => handleSubmit(e)}
+        >
+          Log In
+        </button>
+        <p className="small fw-bold mt-2 pt-1 mb-0">
+          Don't have an account?{" "}
+          <a href="#!" className="link-danger">
+            Register
           </a>
-        </div>
-        <div>
-          <select
-            required
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-              setUserType(e.target.value)
-            }
-          >
-            <option value="" disabled selected>
-              Choose One Please{" "}
-            </option>
-            <option value="doctor">Doctor</option>
-            <option value="patient">Patient</option>
-          </select>
-        </div>
-
-        <div className="text-center text-lg-start mt-4 pt-2">
-          <button
-            type="submit"
-            className="btn btn-primary btn-lg"
-            style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-          >
-            Log In
-          </button>
-          <p className="small fw-bold mt-2 pt-1 mb-0">
-            Don't have an account?{" "}
-            <a href="#!" className="link-danger">
-              Register
-            </a>
-          </p>
-        </div>
-      </form>
+        </p>
+      </div>
     </div>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 import "./style.css"
 import React, { useState } from 'react'
@@ -9,7 +8,7 @@ import TeamMember from "../src/components/TeamMember"
 import Link from 'next/link';
 import Image from "next/image"
 import { useSelector } from "react-redux"
-// import { RootState } from "../../store/store"
+import { RootState } from "./../src/redux/store"
 var obj = {
     Neurologist: {
         para: "A neurologist is a medical doctor who specializes in the diagnosis and treatment of disorders that affect the nervous system. The nervous system is a complex network that includes the brain, spinal cord, and peripheral nerves. Neurologists are experts in the management of various neurological conditions",
@@ -50,7 +49,7 @@ const LandingPage = () => {
     const [department, setDepartment] = useState("")
     const [name, setName] = useState("")
     const isLoggedIn = localStorage.getItem('token');
-    // const { allDoctors } = useSelector((state: RootState) => state.doctor);
+    const { allDoctors } = useSelector((state: RootState) => state.doctor);
 
     return (
         <div className="landing-page-container">
@@ -185,7 +184,7 @@ const LandingPage = () => {
                         er<br /> dalar elementum tempus hac tellus libero accumsan. </p>
                 </div>
                 <div style={{ padding: "0rem 4rem 1rem 7rem" }} className="all-services-cards-container d-flex  flex-wrap gap-4 w-100">
-                    {/* {allDoctors?.map((doctor:object,i:number) => <TeamMember key={i} doctor={doctor} />)} */}
+                    {allDoctors?.map((doctor:object,i:number) => <TeamMember key={i} doctor={doctor} />)}
                   
                 </div>
             </div>
@@ -194,100 +193,3 @@ const LandingPage = () => {
 }
 
 export default LandingPage
-
-import Image from 'next/image'
-import styles from './page.module.css'
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
-
