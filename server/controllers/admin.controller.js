536f135fc78e15f73811b3c3fa18cb32692ee0d1
FediMaalej <fm3aalej@gmail.com>
2023-10-19 21:21:41 +0100

    auth and payment completed

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -64,4 +64,4 @@ exports.signin = async (req, res) => {
       console.error(error);
       res.status(500).json({ message: "Server error" });
     }
-  };
+  }; 
--- server/routers/admin.doc.router.js
+++ server/routers/admin.doc.router.js
@@ -4,12 +4,6 @@ const AdminDocRouter = express.Router();
 
 AdminDocRouter.get("/all", getAll);
 AdminDocRouter.post('/signup', signup);
-
-// Sign-in route
 AdminDocRouter.post('/signin', signin);
-// AdminDocRouter.get("/:id", getOne);
-// AdminDocRouter.post("/add", create);
-// AdminDocRouter.delete("/:id", remove);
-// AdminDocRouter.put("/:id", update);
 
 module.exports = AdminDocRouter;
