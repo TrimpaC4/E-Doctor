a9ea2b6727ddaee0242c317e328ff9c58de3beb3
aymenmakhlouff <makhloufaymen.fr@gmail.com>
2023-10-20 13:24:17 +0100

    save

--- client/app/map/page.tsx
+++ client/app/map/page.tsx
@@ -19,57 +19,58 @@ interface mapStyle{
     zoom: any
 }
 
-const Marker: React.FC<any> = ({ onClick, children, feature }) => {
-  const _onClick = () => {
-    onClick(feature.properties.description);
-  };
-
-  return (
-    <button onClick={_onClick} className="marker">
-      {children}
-    </button>
-  );
-};
 
 const Map = () => {
-  const mapContainerRef:any = useRef(null);
+  const mapContainerRef :any= useRef(null);
 
   // Initialize map when component mounts
   useEffect(() => {
-    const map: any= new mapboxgl.Map({
+    const map :any= new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [-87.65, 41.84],
       zoom: 10,
     });
 
-    // Render custom marker components
-    geoJson.features.forEach((feature:any) => {
-      // Create a React ref
-      const ref:any = React.createRef();
-      // Create a new DOM node and save it to the React ref
-      ref.current = document.createElement('div');
-      // Render a Marker Component on our new DOM node
-      createRoot(ref.current).render(
-        <Marker onClick={markerClicked} feature={feature} />
+    map.on("load", function () {
+      // Add an image to use as a custom marker
+      map.loadImage(
+        "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+        function (error:any, image:string) {
+          if (error) throw error;
+          map.addImage("custom-marker", image);
+          // Add a GeoJSON source with multiple points
+          map.addSource("points", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: geoJson.features,
+            },
+          });
+          // Add a symbol layer
+          map.addLayer({
+            id: "points",
+            type: "symbol",
+            source: "points",
+            layout: {
+              "icon-image": "custom-marker",
+              // get the title name from the source's "title" property
+              "text-field": ["get", "title"],
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 1.25],
+              "text-anchor": "top",
+            },
+          });
+        }
       );
-
-      // Create a Mapbox Marker at our new DOM node
-      new mapboxgl.Marker(ref.current)
-        .setLngLat(feature.geometry.coordinates)
-        .addTo(map);
     });
 
     // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
     // Clean up on unmount
     return () => map.remove();
   }, []);
-
-  const markerClicked = (title:any) => {
-    window.alert(title);
-  };
 
   return <div className="map-container" ref={mapContainerRef} />;
 };
