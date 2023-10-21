9080e15e3af37a444e2f5f707d44fc1a761cc770
AmineDrissi <aminedrissi514@gmail.com>
2023-10-21 10:23:11 +0100

    aa

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       </head>
       <body>
         <ReduxProvider>
-          <NavBar />
+          {/* <NavBar /> */}
           {children}
         </ReduxProvider>
         <script
